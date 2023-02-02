0e137d3ac0026d4c63cf9a156ae0353bb5723fb9
Victor Antunez <victorac94@hotmail.com>
2023-02-02 14:42:56 +0100

    undo update CORS policy

--- app.js
+++ app.js
@@ -13,11 +13,6 @@ var commentsRouter = require('./routes/comments');
 
 var app = express();
 
-const corsOptions = {
-  origin: 'https://bugtracker-1b38f.web.app/',
-  status: 200
-};
-
 require('dotenv').config();
 
 // Connect to the DB
@@ -27,7 +22,7 @@ require('./db');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
