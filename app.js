4c9af576018b976300d1073bbe42f720ebd0d547
Victor Antunez <victorac94@hotmail.com>
2023-02-06 18:52:02 +0100

    Undo all recent CORS commits

--- app.js
+++ app.js
@@ -23,10 +23,6 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(cors());
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  next();
-})
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
